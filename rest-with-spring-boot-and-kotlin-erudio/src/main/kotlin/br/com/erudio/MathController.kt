70290711b8ff1a6dd2fa442ee6890335fc0262f6
joaore10 <joaore1000@gmail.com>
2025-01-12 20:40:21 -0300

    criando a logica das funcoes secao 06 aula 04

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/MathController.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/MathController.kt
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
@@ -15,6 +16,15 @@ class MathController {
     fun sum(@PathVariable(value = "numberOne") numberOne: String?,
             @PathVariable(value = "numberTwo") numberTwo: String?
     ): Double {
-        return 1.0
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
+        return convertToDouble(numberOne) + convertToDouble(numberTwo)
+    }
+
+    private fun convertToDouble(numberOne: String?): Double {
+        return 0.0
+    }
+
+    private fun isNumeric(numberOne: String?): Boolean {
+        return false
     }
 }
