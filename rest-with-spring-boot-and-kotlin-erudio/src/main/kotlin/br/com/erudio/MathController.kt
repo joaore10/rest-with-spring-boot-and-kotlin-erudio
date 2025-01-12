f82b4c062337ef8cdaa55ef8023a81de1b1831a3
joaore10 <joaore1000@gmail.com>
2025-01-12 20:28:34 -0300

    commit inicial da secao 06 aula 03

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/MathController.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/MathController.kt
@@ -1,17 +1,20 @@
 package br.com.erudio
 
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
-class GreetingController {
+class MathController {
 
     val counter: AtomicLong = AtomicLong()
 
-    @RequestMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String?): Greeting {
-        return Greeting(counter.incrementAndGet(), content = "Hello, $name!")
+    @RequestMapping(value=["/sum/{numberOne}/{numberTwo}"])
+    fun sum(@PathVariable(value = "numberOne") numberOne: String?,
+            @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double {
+        return 1.0
     }
 }
