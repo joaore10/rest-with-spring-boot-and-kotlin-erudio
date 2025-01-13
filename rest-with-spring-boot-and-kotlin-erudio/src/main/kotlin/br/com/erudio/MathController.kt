f022970943c14d26bdf6c29446c97633b07722f6
joaore10 <joaore1000@gmail.com>
2025-01-13 00:06:36 -0300

    Criando e Tratando Exception Secao 06 aula 05

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/MathController.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/MathController.kt
@@ -1,30 +1,31 @@
 package br.com.erudio
 
+import br.com.erudio.exceptions.UnsupportedMathOperationException
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.lang.Exception
-import java.util.concurrent.atomic.AtomicLong
 
 @RestController
 class MathController {
-
-    val counter: AtomicLong = AtomicLong()
 
     @RequestMapping(value=["/sum/{numberOne}/{numberTwo}"])
     fun sum(@PathVariable(value = "numberOne") numberOne: String?,
             @PathVariable(value = "numberTwo") numberTwo: String?
     ): Double {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw UnsupportedMathOperationException("Por favor, preencha com um valor numérico.")
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
     }
 
-    private fun convertToDouble(numberOne: String?): Double {
-        return 0.0
+    private fun convertToDouble(strNumber: String?): Double {
+        if (strNumber.isNullOrBlank()) return 0.0
+        val number = strNumber.replace(",".toRegex(), replacement = ".")
+        return if(isNumeric(number)) number.toDouble() else 0.0
     }
 
-    private fun isNumeric(numberOne: String?): Boolean {
-        return false
+    private fun isNumeric(strNumber: String?): Boolean {
+        if (strNumber.isNullOrBlank()) return false
+        val number = strNumber.replace(",".toRegex(), replacement = ".")
+        return number.matches(regex = """[-+]?[0-9]*\.?[0-9]+""".toRegex())
     }
 }
---
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/exceptions/UnsupportedMathOperationException.kt
@@ -0,0 +1,3 @@
+package br.com.erudio.exceptions
+
+class UnsupportedMathOperationException(exception: String?): RuntimeException(exception)
