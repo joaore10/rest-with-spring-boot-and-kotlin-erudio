0caeb862a460ab8e1c52c7e1852913b757dde4bf
joaore10 <joaore1000@gmail.com>
2025-02-01 13:35:40 -0300

    Migrations com Flyway, adicionando as migrations e a dependencia - Aula 1103

---
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+--
+-- Dumping data for table `person`
+--
+INSERT INTO `person` (`id`, `address`, `first_name`, `genero`, `last_name`) VALUES
+(2,'Campinas 22','Ariane','Female','Gachett'),
+(4,'Campinas 22','Gateu','Female','Festa'),
+(5,'India','Mohamed','Macho','Ali'),
+(6,'Brasil','Larissa','Femea','Manoela'),
+(8,'Brasil - Campinas','Joao','Macho','Ré'),
+(9,'Brasil - Campinas','Osmair Ré','Macho','Junior');
