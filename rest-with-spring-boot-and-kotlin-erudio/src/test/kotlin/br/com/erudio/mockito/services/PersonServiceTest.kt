230c5d1ef16d219ccc2f60401f08932f8de11a6f
joaore10 <joaore1000@gmail.com>
2025-02-09 11:05:35 -0300

    Testando a geracao dos links hateoas no service com mockito e junit5 - Aula 1406

---
+++ rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/mockito/services/PersonServiceTest.kt
@@ -0,0 +1,71 @@
+package br.com.erudio.mockito.services
+
+import br.com.erudio.repository.PersonRepository
+import br.com.erudio.services.PersonService
+import br.com.erudio.unittests.mapper.mocks.MockPerson
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+internal class PersonServiceTest {
+
+    private lateinit var inputObject: MockPerson
+
+    @InjectMocks
+    private lateinit var service: PersonService
+
+    @Mock
+    private lateinit var repository: PersonRepository
+
+    @BeforeEach
+    fun setUpMock() {
+      inputObject = MockPerson()
+      MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun findById() {
+     val person = inputObject.mockEntity(1)
+     person.id = 1L
+     `when`(repository.findById(1)).thenReturn(Optional.of(person))
+
+     val result = service.findById(1)
+
+     assertNotNull(result)
+     assertNotNull(result.key)
+     assertNotNull(result.links)
+     assertTrue(result.links.toString().contains("</api/person/v1/1>;rel=\"self\""))
+     assertEquals("Endereço Test1",result.address)
+     assertEquals("Primeiro Nome Test1",result.firstName)
+     assertEquals("Sobrenome Test1",result.lastName)
+     assertEquals("Femea",result.genero)
+    }
+
+    @Test
+    fun create() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
