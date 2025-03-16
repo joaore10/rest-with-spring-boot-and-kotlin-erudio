f0fcd577d7fb33b7194e631cea114f0bdc087b74
joaore10 <joaore1000@gmail.com>
2025-03-16 00:33:57 -0300

    NOT WORK - Implementando Controller para nossa Authenticação - Aula 1712

---
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controllers/AuthController.kt
@@ -0,0 +1,26 @@
+package br.com.erudio.controllers
+
+import br.com.erudio.data.vo.v1.AccountCredentialsVO
+import br.com.erudio.services.AuthService
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Tag(name = "Authentication Endpoint")
+@RestController
+@RequestMapping("/auth")
+class AuthController {
+
+    @Autowired
+    lateinit var authService: AuthService
+
+    fun signin(@RequestBody data: AccountCredentialsVO?) : ResponseEntity<*>{
+        return if(data!!.username.isNullOrBlank() || data.password.isNullOrBlank())
+            ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request")
+            else authService.signin(data)
+    }
+}
